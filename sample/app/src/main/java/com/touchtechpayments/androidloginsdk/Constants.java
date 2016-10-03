0af1afce42d7b04906369ce618ab10ce3b1041b3
Gavin Pacini <gavin.pacini@gmail.com>
2016-10-03 15:52:17 +0100

    Update API KEY

--- sample/app/src/main/java/com/touchtechpayments/androidloginsdk/Constants.java
+++ sample/app/src/main/java/com/touchtechpayments/androidloginsdk/Constants.java
@@ -4,5 +4,5 @@ package com.touchtechpayments.androidloginsdk;
  * Created by gavinpacini on 15/08/2016.
  */
 public class Constants {
-    public static final String API_KEY = "w5ihnxJ4yVp051MH1234";
+    public static final String API_KEY = "APIKEY";
 }
